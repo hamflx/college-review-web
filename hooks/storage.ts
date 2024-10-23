3b0c8c931fa36c37a6204b3cbb4f9c35ffebf60d
hamflx <a@hamflx.cn>
2024-10-23 21:51:45 +0800

    修复 localStorage is undefined 的问题。

--- hooks/storage.ts
+++ hooks/storage.ts
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useLocalStorage = <T>(
   key: string,
   initialValue: T,
 ): [T, (value: T) => void] => {
-  const cachedValue = localStorage.getItem(key);
-  const [value, setValue] = useState(
-    cachedValue ? JSON.parse(cachedValue) : initialValue,
-  );
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    const cachedValue = localStorage.getItem(key);
+
+    if (cachedValue) {
+      setValue(JSON.parse(cachedValue));
+    }
+  }, []);
+
   const setValueAndLocalStorage = (newValue: T) => {
     localStorage.setItem(key, JSON.stringify(newValue));
     setValue(newValue);
